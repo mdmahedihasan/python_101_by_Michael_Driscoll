aea11396f5eea3951bb4aeaa76f12f6b7ad19ff8
mahedi <mahedi_h@yahoo.com>
2017-05-02 03:48:44 +0600

    finish 26

---
+++ 26_The_lambda/test1.py
@@ -0,0 +1,9 @@
+import math
+
+
+def sqroot(x):
+    """find the square root of the number passed in"""
+    return math.sqrt(x)
+
+
+print(sqroot(9))
---
+++ 26_The_lambda/test2.py
@@ -0,0 +1,6 @@
+import math
+
+
+square_root = lambda x: math.sqrt(x)
+
+print(square_root(9))
